00c8bde542918a69d8b7a49bbd2b0cf4363597bf
Mayur Charniya <mayurcharniya1612@outlook.com>
2020-08-08 20:09:33 +0530

    commnet not display

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap"
 
 
-class DishDetail extends Component {
 
-    renderDish(dish) {
 
-        if (dish != null) {
+
+
+
+
+    function RenderDish({dish}) {
+
             return (
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
@@ -15,8 +18,34 @@ class DishDetail extends Component {
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-            )
+            );
         }
+
+
+    function RenderComments({comments}) {
+        if (comments != null)
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4> <em><b>Comments</b></em> </h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comments}</p>
+                                    <p>-- {comment.author},
+                                        &nbsp;
+                                        {new Intl.DateTimeFormat('en-US', {
+                                        year: 'numeric',
+                                        month: 'short',
+                                        day: '2-digit'
+                                    }).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+           );
         else {
             return (
                 <div></div>
@@ -24,54 +53,26 @@ class DishDetail extends Component {
         }
     }
 
-    renderComments(dish) {
-        if (dish != null) {
-            const comments = dish.comments.map((comment) => {
-                return (
-                    <li key={comment.id}>
-                        <p>-- {comment.author},
-                            &nbsp;
-                            {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'short',
-                            day: '2-digit'
-                        }).format(new Date(Date.parse(comment.date)))}
-                        </p>
-                        <p>{ comment.comment }</p>
-                    </li>
-                )
-            })
-            return comments;
-        }
-        else {
+        const DishDetail = (props) => {
+
+        if (props.dish != null)
             return (
-                <div>
-                    No Comments.
+                <div className='container' >
+                    <div className='row' >
+                        <div className='col-md-5 col-sm-12 m-1' >
+                            <RenderDish dish={props.dish} />
+                            <RenderComments comments={props.comments} />
+                        </div>
+                    </div>
                 </div>
+            )
+        else {
+            return(
+                <div></div>
             )
         }
     }
 
-
-    render() {
-
-        return (
-            <div className='container' >
-                <div className='row' >
-                    <div className='col-md-5 col-sm-12 m-1' >
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4> <em><b>Comments</b></em> </h4>
-                        <ul className='list-unstyled'>
-                            {this.renderComments(this.props.dish)}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
 
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,19 +2,24 @@ import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 
-class Menu extends Component {
+    function RenderMenuItem({ dish, onclick }) {
+        return(
+            <Card key={dish.id} onClick={() => onclick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay body className='ml-5'>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
+    }
 
-    render() {
 
-        const Menu = this.props.dishes.map((dish) => {
+    const Menu = (props) => {
+
+        const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1" >
-                    <Card key={dish.id} onClick={() => this.props.onclick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay body className='ml-5'>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <RenderMenuItem dish={dish} onclick={props.onclick}/>
                 </div>
             );
         });
@@ -22,12 +27,13 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row" >
-                    {Menu} {}
+                    {menu} 
                 </div>
             </div>
         );
     }
-}
+
+
 
 
 export default Menu;
