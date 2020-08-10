d432f3e90d3cf153b18819450ff71ce11583c1f8
Mayur Charniya <mayurcharniya1612@outlook.com>
2020-08-10 21:03:15 +0530

    Manav check error

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
-
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navlink } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
             <React.Fragment>
-                <Navbar dark >
+                <Navbar dark expand="md">
                     <div className='container'>
-                        <NavbarBrand href="/">Restaurante con fusión</NavbarBrand>
+                        <NavbarBrand className='mr-auto' href="/">
+                            <img src="assets/logo.png" height="30" width="41" alt="Restaurante con fusión"/>
+                        </NavbarBrand>
+                        <Nav navbar >
+                            <NavItem>
+                                <Navlink className="nav-link" to ="/home">
+                                    <span className="fa fa-home fa-lg "> Home </span>
+                                </Navlink>
+                            </NavItem>
+                            <NavItem>
+                                <Navlink className="nav-link" to ="/aboutus">
+                                    <span className="fa fa-info fa-lg "> Aboutus </span>
+                                </Navlink>
+                            </NavItem>
+                            <NavItem>
+                                <Navlink className="nav-link" to ="/menu">
+                                    <span className="fa fa-list fa-lg "> Menu </span>
+                                </Navlink>
+                            </NavItem>
+                            <NavItem>
+                                <Navlink className="nav-link" to ="/contactus">
+                                    <span className="fa fa-address-card fa-lg "> Contact Us </span>
+                                </Navlink>
+                            </NavItem>
+                        </Nav>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Home from "./HomeComponent";
 import Menu from './MenuComponent';
 import DishDetail from "./DishdetailComponent.js";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent"
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
 
@@ -13,10 +14,7 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     };
-
-    this.onDishSelect = this.onDishSelect.bind(this);
   }
 
   onDishSelect(dishId) {
@@ -25,12 +23,21 @@ class Main extends Component {
 
 
   render() {
+
+    const HomePage = () => {
+      return(
+        <Home/>
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-          onclick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Redirect to="/home"/>
+        </Switch>
         <Footer />
       </div>
     );
