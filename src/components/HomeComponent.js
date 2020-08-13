181c36f1e6ca8a1b50fe72b9a89c0700ce3c4018
Manav Parmar <parmar.manav2000@gmail.com>
2020-08-13 11:32:23 +0530

    solved

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 
-function RenderCard({item}) {
+function RenderCard({ item }) {
 
-    return(
+    return (
         <Card>
             <CardImg src={item.image} alt={item.name} />
             <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-            <CardText>{item.description}</CardText>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
     );
@@ -18,14 +20,14 @@ function RenderCard({item}) {
 }
 
 function Home(props) {
-    return(
+    return (
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.dish} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotions} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
